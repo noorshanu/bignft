190a9fec249856c8ec8096de0c49cb8383b76a0c
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-12-17 12:14:02 +0530

    dfggf

--- src/Components/Hero/Card.js
+++ src/Components/Hero/Card.js
@@ -1,38 +1,32 @@
-import React from 'react'
-import Clock from '../../assets/timer.png'
+import React from "react";
+import Clock from "../../assets/timer.png";
 
 function Card() {
   return (
     <>
-    <div className='card-box'>
-        <div className='card-content'>
-            <h1>BUY $BIGNFTS ON PRESALE</h1>
-            <p>1 $BIGNFTS = $1</p>
+      <div className="card-box">
+        <div className="card-content">
+          <h1>BUY $BIGNFTS ON PRESALE</h1>
+          <p>1 $BIGNFTS = $1</p>
 
-            <div className='bar'>
-                <div>
-                    bar
-                </div>
-                <p>Progress <span>48%</span>(23,750,345/50,000,000)</p>
-                <div className='d-flex'>
-                    <a href="/">Connect Wallet</a>
-                    <a href="/"> HOW TO BUY</a>
-
-                </div>
-                <div className='d-flex'>
-                    <img src={Clock} alt=""/>
-                    <p>10 DAYS, 15 HOURS, 43 MINUTES, 05 SECONDS remaining</p>
-
-                </div>
-
+          <div className="bar">
+            <div>bar</div>
+            <p>
+              Progress <span>48%</span>(23,750,345/50,000,000)
+            </p>
+            <div className="d-flex">
+              <a href="/">Connect Wallet</a>
+              <a href="/"> HOW TO BUY</a>
             </div>
-
+            <div className="d-flex">
+              <img src={Clock} alt="" />
+              <p>10 DAYS, 15 HOURS, 43 MINUTES, 05 SECONDS remaining</p>
+            </div>
+          </div>
         </div>
-
-
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Card
+export default Card;
--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,18 +1,15 @@
-import React from 'react'
-import Card from './Card'
-import './Hero.css'
+import React from "react";
+import Card from "./Card";
+import "./Hero.css";
 
 function Hero() {
   return (
-    <section className='main'>
-        <div className='container justify-content-center align-items-center'>
-
-<Card/>
-
-        </div>
-        
+    <section className="main">
+      <div className="container justify-content-center align-items-center">
+        <Card />
+      </div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
