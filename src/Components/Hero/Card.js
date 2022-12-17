60d341a8b8a0e0e9209b8716b164061ff00f65b2
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-12-17 12:31:39 +0530

    dfgdf

--- src/Components/Hero/Card.js
+++ src/Components/Hero/Card.js
@@ -7,18 +7,18 @@ function Card() {
       <div className="card-box">
         <div className="card-content">
           <h1>BUY $BIGNFTS ON PRESALE</h1>
-          <p>1 $BIGNFTS = $1</p>
+          <p className="symbol">1 $BIGNFTS = $1</p>
 
           <div className="bar">
             <div>bar</div>
             <p>
               Progress <span>48%</span>(23,750,345/50,000,000)
             </p>
-            <div className="d-flex">
-              <a href="/">Connect Wallet</a>
-              <a href="/"> HOW TO BUY</a>
+            <div className="d-flex justify-content-center gap-4">
+              <a href="/" className="connect-btn">Connect Wallet</a>
+              <a href="/" className="how"> HOW TO BUY</a>
             </div>
-            <div className="d-flex">
+            <div className="d-flex justify-content-center ">
               <img src={Clock} alt="" />
               <p>10 DAYS, 15 HOURS, 43 MINUTES, 05 SECONDS remaining</p>
             </div>
